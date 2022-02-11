2d3282a6a706ce47ab9b390a97c139cfc2ccbfa4
Donggyun Ha <irvcaptain@knu.ac.kr>
2022-02-12 00:13:10 +0900

    getReadyChapter7

--- movie_app/src/App.js
+++ movie_app/src/App.js
@@ -1,4 +1,29 @@
 import { useState, useEffect } from "react";
+/*
+function Hello() {
+  function byeFn() {
+    console.log("bye ;(");
+  }
+  function hiFn() {
+    console.log("hi :)");
+    return byeFn;
+  }
+  useEffect(hiFn, []);
+  return <h1>Hello</h1>;
+}
+*/
+/*
+function App() {
+  const [showing, setShowing] = useState(false);
+  const onClick = () => setShowing((prev) => !prev);
+  return (
+    <div>
+      {showing ? <Hello /> : null}
+      <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
+    </div>
+  );
+  }
+  */
 
 function App() {
   const [counter, setValue] = useState(0);
